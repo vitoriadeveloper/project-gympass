8384b429f0949a36dd96438729cfad590683f61d
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-03-28 22:52:00 -0300

    initial config

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
-import { app } from "./app"
-import { env } from "./env"
-
-
+import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: env.PORT,
 }).then(() => {
-    console.log('🚀HTTP Server running🚀');
-
-})
+    console.log("🚀HTTP Server running🚀");
+});
