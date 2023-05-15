1468f144745c72f27498ca5e6c43485f84990aff
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-05-15 14:35:53 -0300

    controllers finished

--- src/http/controllers/gyms/nearby.spec.ts
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -12,7 +12,7 @@ describe("Nearby Gym (e2e)", () => {
         await app.close();
     });
     it("should be able to list nearby gyms", async () => {
-        const { token } = await createAndAuthenticateUser(app);
+        const { token } = await createAndAuthenticateUser(app, true);
 
         await request(app.server)
             .post("/gyms")
